a0d8a924010d9818b360e74e360e2959858e80fa
jason-choi-skku <jasonchoi3@g.skku.edu>
2022-11-18 16:06:06 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: "/",
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>SKKU OSSP Galleryyy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      Hello World
       
     </div>
   )
