8341ed5ba5bdaeeb89358322670a31a16f5facc0
jason-choi-skku <jasonchoi3@g.skku.edu>
2022-11-22 11:42:42 +0900

    [REMOVE] recoil, [ADD] Rajdhandi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
-import theme from "../styles";
 import "pretendard/dist/web/variable/pretendardvariable.css";
-import { Suspense } from "react";
+import "@fontsource/rajdhani";
+
+import theme from "../styles";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <RecoilRoot>
-            <ChakraProvider theme={theme}>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Component {...pageProps} />
-                </Suspense>
-            </ChakraProvider>
-        </RecoilRoot>
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,50 @@
-import {
-    Container, SimpleGrid
-} from "@chakra-ui/react";
+import { Box, Container, SimpleGrid } from "@chakra-ui/react";
+import axios from "axios";
 import Head from "next/head";
-import { useRecoilValue } from "recoil";
+import { useEffect, useState } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Preview } from "../components/Preview";
-import { dataSelector } from "../states";
+import { Project } from "../types";
 
 export default function Home() {
-    const data = useRecoilValue(dataSelector);
+    const [dataState, setDataState] = useState<Project[]>([]);
+    const [loadingState, setLoadingState] = useState<boolean>(true);
+
+    useEffect(() => {
+        (async () => {
+            const { data }: { data: Project[] } = await axios.get(
+                "https://raw.githubusercontent.com/Jason-Choi/skku-ossp-gallery/main/data/mockup.json"
+            );
+            setDataState(data);
+            setLoadingState(false);
+        })();
+    }, []);
+
     return (
         <>
             <Head>
                 <title>SKKU OSSP Gallery</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Container maxW={{ base: "container.sm", xl: "container.xl" }}>
-                <SimpleGrid my={20} columns={{ base: 2, xl: 4 }} spacing="10">
-                    <Preview project={data[0]}></Preview>
-                    <Preview project={data[0]}></Preview>
-                    <Preview project={data[0]}></Preview>
-                    <Preview project={data[0]}></Preview>
-                    <Preview project={data[0]}></Preview>
-                    <Preview project={data[0]}></Preview>
-                </SimpleGrid>
-            </Container>
-
+            {loadingState ? (
+                <Box>Loading</Box>
+            ) : (
+                <Container maxW={{ base: "container.sm", xl: "container.xl" }}>
+                    <SimpleGrid
+                        my={20}
+                        columns={{ base: 2, xl: 4 }}
+                        spacing="10"
+                    >
+                        <Preview project={dataState[0]}></Preview>
+                        <Preview project={dataState[0]}></Preview>
+                        <Preview project={dataState[0]}></Preview>
+                        <Preview project={dataState[0]}></Preview>
+                        <Preview project={dataState[0]}></Preview>
+                        <Preview project={dataState[0]}></Preview>
+                    </SimpleGrid>
+                </Container>
+            )}
             <Header />
             <Footer />
         </>
