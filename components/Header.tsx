ca62c6cc22d682c58726436b19301d180eace4e6
jason-choi-skku <jasonchoi3@g.skku.edu>
2022-12-05 14:40:52 +0900

    [ADD] header survey button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,48 +1,95 @@
-import { Button, Center, Flex, Stack, Text } from "@chakra-ui/react"
-import { AiFillGithub } from "react-icons/ai"
-import { ImLab } from "react-icons/im"
-import SKKULogo from "../public/skku.svg"
+import { Button, Center, Flex, Stack, Text } from "@chakra-ui/react";
+import { AiFillGithub } from "react-icons/ai";
+import { RiSurveyFill } from "react-icons/ri";
+import { ImLab } from "react-icons/im";
+import SKKULogo from "../public/skku.svg";
 
 export const Header = () => {
     return (
-        <Center as="nav" role={"navigation"} minH="16" width="full" px={4} position={"fixed"} top={0} backgroundColor="white" boxShadow={"xs"}>
-            <Stack direction={"row"} mt={1} w={{ base: "full", xl: "container.xl" }} justifyContent={"space-between"} verticalAlign="center">
+        <Center
+            as="nav"
+            role={"navigation"}
+            minH="16"
+            width="full"
+            px={4}
+            position={"fixed"}
+            top={0}
+            backgroundColor="white"
+            boxShadow={"xs"}
+        >
+            <Stack
+                direction={"row"}
+                mt={1}
+                w={{ base: "full", xl: "container.xl" }}
+                justifyContent={"space-between"}
+                verticalAlign="center"
+            >
                 <Center>
                     <SKKULogo width="30" />
                     <Flex direction={"column"} marginLeft={4}>
-                        <Text fontSize={"sm"} color="gray.500">
+                        <Text
+                            fontSize={{ base: "xs", sm: "md" }}
+                            color="gray.500"
+                        >
                             성균관대학교 소프트웨어학과
                         </Text>
-                        <Text fontSize={"md"} fontWeight={600} color="gray.500">
+                        <Text
+                            fontSize={{ base: "sm", sm: "md" }}
+                            fontWeight={600}
+                            color="gray.500"
+                        >
                             오픈소스 소프트웨어 실습
                         </Text>
                     </Flex>
                 </Center>
                 <Center>
-                    <Stack direction={{ base: "column", sm: "row" }} spacing={0} align="start">
-                        <Button py={3} variant={"ghost"} leftIcon={<ImLab />} size={{ base: "sm", md: "xl" }} px={4} onClick={() => window.open("https://idclab.skku.edu")}>
+                    <Stack direction={{ base: "row", sm: "row" }} align="start">
+                        <Button
+                            py={4}
+                            variant={"solid"}
+                            colorScheme="gray"
+                            leftIcon={<ImLab />}
+                            size={{ base: "xs", md: "sm" }}
+                            px={4}
+                            onClick={() =>
+                                window.open("https://idclab.skku.edu")
+                            }
+                        >
                             <Flex>
-                                <Text fontSize={"lg"} fontFamily="idc" fontWeight={600}>
+                                <Text
+                                    fontSize={"md"}
+                                    fontFamily="idc"
+                                    fontWeight={600}
+                                >
                                     IDC
                                 </Text>
-                                <Text fontSize={"lg"} fontFamily="idc" fontWeight={500}>
+                                <Text
+                                    fontSize={"md"}
+                                    fontFamily="idc"
+                                    fontWeight={500}
+                                >
                                     Lab
                                 </Text>
                             </Flex>
                         </Button>
                         <Button
-                            py={3}
-                            variant={"ghost"}
-                            leftIcon={<AiFillGithub />}
-                            size={{ base: "sm", md: "xl" }}
+                            py={4}
+                            variant={"solid"}
+                            colorScheme="green"
+                            leftIcon={<RiSurveyFill />}
+                            size={{ base: "xs", md: "sm" }}
                             px={4}
-                            onClick={() => window.open("https://github.com/jason-choi/skku-ossp-gallery")}
+                            onClick={() =>
+                                window.open(
+                                    "https://docs.google.com/forms/d/e/1FAIpQLSfu9JBNfNYu4KXPRTCnULin6Zg70BqsKsbFYz3IZbYU3wFv7g/viewform"
+                                )
+                            }
                         >
-                            Github
+                            Survey
                         </Button>
                     </Stack>
                 </Center>
             </Stack>
         </Center>
-    )
-}
+    );
+};
